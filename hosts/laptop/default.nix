ee633e383219696b4cd0f4e802b477c04a859684
Hannes Lundh <hanneslundh1996@gmail.com>
2025-05-11 01:04:02 +0200

    enable tlp

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -25,7 +25,7 @@
   networking.networkmanager.enable = true;
 
   services = {
-    power-profiles-daemon.enable = true;
+    # power-profiles-daemon.enable = true;
     auto-epp.enable = true;
 
     xserver.videoDrivers = [ "nvidia" ];
@@ -39,18 +39,18 @@
       criticalPowerAction = "PowerOff";
     };
 
+    tlp.enable = true;
     tlp.settings = {
-      CPU_ENERGY_PERF_POLICY_ON_AC = "power";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
-      CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 1;
-
-      CPU_HWP_DYN_BOOST_ON_AC = 1;
-      CPU_HWP_DYN_BOOST_ON_BAT = 1;
-
-      PLATFORM_PROFILE_ON_AC = "performance";
-      PLATFORM_PROFILE_ON_BAT = "performance";
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
     };
   };
 
