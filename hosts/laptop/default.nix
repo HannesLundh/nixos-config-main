19f708c574c01cefd1f7550bb0dfcccb0345a5b7
Hannes Lundh <hanneslundh1996@gmail.com>
2025-06-20 11:26:54 +0200

    changing tlp settings

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -55,7 +55,7 @@
       CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
 
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "balance-performance";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "power";
 
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
